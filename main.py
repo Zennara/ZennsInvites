67e5353d43f95f5f82e0686154cbb529451179bf
Keagan Landfried <keaganlandfried@gmail.com>
2020-09-24 03:52:44 +0000

    Set Up Hosting

---
+++ main.py
@@ -0,0 +1,6 @@
+import keep_alive
+
+
+#end
+keep_alive.keep_alive() 
+#keep the bot running after the window closes, use UptimeRobot to ping the website at least every <60min. to prevent the website from going to sleep, turning off the bot
