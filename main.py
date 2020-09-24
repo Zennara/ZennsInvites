e852fff70a92e14b7389f8b480aa9914881bc6e3
Keagan Landfried <keaganlandfried@gmail.com>
2020-09-24 04:02:40 +0000

    Setup Bot

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import keep_alive
+import discord
+import os
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("-----------------")
+    print(client.user)
+    print("-----------------")
 
 #end
 keep_alive.keep_alive() 
 #keep the bot running after the window closes, use UptimeRobot to ping the website at least every <60min. to prevent the website from going to sleep, turning off the bot
+
+#run bot
+client.run(os.environ.get("DISCORD_BOT_SECRET"))
