c7be3dd6e78e58644000b2dbff14c532def8a304
Keagan Landfried <keaganlandfried@gmail.com>
2020-09-25 18:12:56 +0000

    Made !info display name and id

--- main.py
+++ main.py
@@ -3,16 +3,11 @@ import discord
 import os
 import asyncio
 import json
+from discord.utils import get
 
 client = discord.Client()
 
-#f = open('invites.json')
-#invites = json.load(f)
-#f.close()
-
-
 guild_id = "696583117502152774"
-logs_channel = "696589280344145991"
 invites = {}
 last = ""
 
@@ -20,9 +15,9 @@ async def fetch():
  global last
  global invites
  global codeOwner
+ global joinCode
  await client.wait_until_ready()
  gld = client.get_guild(int(guild_id))
- logs = client.get_channel(int(logs_channel))
  while True:
   invs = await gld.invites()
   tmp = []
@@ -30,19 +25,16 @@ async def fetch():
    for s in invites:
     if s[0] == i.code:
      if int(i.uses) > s[1]:
-      usr = gld.get_member(int(last))
-      testh = f"{usr.name} **joined**; Invited by **{i.inviter.name}** (**{str(i.uses)}** invites)"
-      await logs.send(testh)
-
       #get inviter id
       codeOwner = str(i.inviter.id)
+      joinCode = str(i.code)
    tmp.append(tuple((i.code, i.uses)))
   invites = tmp
   await asyncio.sleep(4)
 
 @client.event
 async def on_ready():
-  print("\nZennInvites Ready")
+  print("\nZennInvites Ready\n")
     
 @client.event
 async def on_message(message):
@@ -53,33 +45,44 @@ async def on_message(message):
                 totalInvites += i.uses
         await message.channel.send("You've invited " + str(totalInvites) + " members(s) to the server!")
 
-    #if message.content.startswith('!db'):
+    if message.content.startswith('!info '):
+        user = message.guild.get_member(message.mentions[0].id)
+        embedVar = discord.Embed(title=user.name, color=0x8a0303)
+        embedVar.add_field(name="ID:", value=message.mentions[0].id, inline=False)
+        embedVar.add_field(name="Date Created:", value=user.created_at, inline=False)
+        await message.channel.send(embed=embedVar)
       
 
 @client.event
 async def on_member_join(member):
   global last
+  global lastName
   last = str(member.id)
+  lastName = str(member.name)
 
   #wait until fetch() is done
   await asyncio.sleep(5)
 
-  #count invites
+  #declare users{}
   with open("database.json", 'r') as f:
     users = json.load(f)
     f.close()
-  with open("database.json", 'w') as f:
-    if codeOwner not in users:
-      users[codeOwner] = {'invites': 0, 'leaves': 0}
-    users[codeOwner]['invites'] += 1
-    f.close()
+
+  #append join code
+  if str(member.id) not in users:
+    users[str(member.id)] = {'invites': 0, 'leaves': 0, 'joinCode': joinCode}
+  users[str(member.id)]['joinCode'] = joinCode
+
+  #add to invites
+  if codeOwner not in users:
+    users[codeOwner] = {'invites': 0, 'leaves': 0, 'joinCode': "null"}
+  users[codeOwner]['invites'] += 1
 
   #write new data to files
   with open("database.json", 'w') as f:
     json.dump(users, f)
     f.close()
 
-#client.loop.create_task(PushInvites())
 client.loop.create_task(fetch())
 
 keep_alive.keep_alive() 
